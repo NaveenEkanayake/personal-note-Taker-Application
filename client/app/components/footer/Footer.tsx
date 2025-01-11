8a84b9f16f24638577099ea0dd1c3ab3806f714a
Naveen Ekanayake <157974683+NaveenEkanayake@users.noreply.github.com>
2025-01-11 17:08:37 +0530

    home com

--- client/app/components/footer/Footer.tsx
+++ client/app/components/footer/Footer.tsx
@@ -7,7 +7,7 @@ import twitterIcon from "../../../public/assests/Twitter.png";
 
 export default function Footer() {
   return (
-    <footer className="bg-black py-8">
+    <div className="bg-black py-8">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           <div className="space-y-4 text-center md:text-left">
@@ -64,6 +64,6 @@ export default function Footer() {
           </p>
         </div>
       </div>
-    </footer>
+    </div>
   );
 }
